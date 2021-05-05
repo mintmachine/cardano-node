7da5b48bb43268eee806ef28e883c6831e692880
allan <allan.w.hart@gmail.com>
2021-05-05 10:01:18 -0600

    Added IOHK official docker-compose for node and wallet

--- start-relay.sh
+++ start-relay.sh
@@ -4,6 +4,5 @@
  --topology /config/testnet-topology.json \
  --database-path /data/db \
  --socket-path /ipc/node.socket \
- --host-addr 0.0.0.0 \
  --port 3001 \
  --config /config/testnet-config.json
